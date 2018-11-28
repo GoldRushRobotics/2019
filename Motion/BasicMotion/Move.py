5ec72a571c84d081211f5f31fa1467528d765624
car-robotics <45017635+car-robotics@users.noreply.github.com>
2018-11-27 19:50:58 -0500

    Updated Move
    Using new method of communication.

--- Motion/BasicMotion/Move.py
+++ Motion/BasicMotion/Move.py
@@ -1,31 +1,17 @@
-import smbus2 as smbus
+import smbus as smbus
 import time
 
 bus = smbus.SMBus(1)
 
-slave_address = 0x04
+slave_address = 0x08
 
-def writeNumber(value):
-    bus.write_byte(slave_address, value)
-    # bus.write_byte_data(address, 0, value)
+def writeArray(value):
+    bus.write_i2c_block_data(slave_address, 0, value)
     return -1
 
-def sendMessage(msg):
-    data = msg
-    data_list = list(data)
+#while 1:
+writeArray([ord('w'),200,1])
+time.sleep(3)
 
-    for i in data_list:
-        # Sends to the Slaves
-        writeNumber(int(ord(i)))
-        time.sleep(.1)
-    writeNumber(int(0x0A))
-
-#loop (4)
-#drive forward
-#turn left
-
-#turn 180
-
-#loop (4)
-#drive forward
-#turn right
+writeArray([ord('f'),0,127])
+#time.sleep(5)
