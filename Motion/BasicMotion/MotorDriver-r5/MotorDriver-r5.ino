0f18dd50f60e6b798b03fce7fec9fbe04d2e9e65
Matt <mattwells19@users.noreply.github.com>
2019-02-02 13:45:10 -0500

    Motor Control Update
    1) Fixed Arduino Folder error
    2) Implemented WASD-Z drive control
    3) Read Int from Serial

--- Motion/BasicMotion/MotorDriver-r5/MotorDriver-r5.ino
+++ Motion/BasicMotion/MotorDriver-r5/MotorDriver-r5.ino
@@ -1,6 +1,7 @@
 
 // R3: Jan 23, 2019: Basic functionality     -Greg Lewis
 // R4: Jan 23, 2019: Smooth control         -Greg Lewis
+// R5L Feb 2, 2019: WASD and Int implementation over serial   -Matt Wells
 
 #define rightSpd 3  // PWM Magnitude
 #define rightDir 2  // Digital direction
@@ -9,17 +10,12 @@
 #define feedGND 6
 #define feedHOT 7
 
-int speed = 128;     //0 = velocity, 1 = turn
-int direction = 128;
+int speed = 0;     //0 = velocity, 1 = turn
+int direction = 0;
 int speedStep = 12;
 int directionStep = 12;
-char vl = 0;        //Shortcut for velocity
-char tn = 1;        //Shortcut for turn
 
 void move(int velocity, int turn);  //Declare the main movement funtion
-void moveChar(int velocityChar, int turnChar);
-void halt();
-void m();
 
 void setup(void)
 {
@@ -42,50 +38,37 @@ void loop(void){
   int val = 0;
   char mode = 0;
 
+  while(Serial.available() <= 0);
+
   do {
     mode = Serial.read();
-  } while(mode != 's' && mode != 't');
+  } while(mode != 'w' && mode != 'a' && mode != 's' && mode != 'd' && mode != 'z');
 
-    
-  Serial.println(mode);
-  Serial.read();
-  Serial.read();
+  while(Serial.available() <= 0);
 
-  do {
-    val = Serial.read();
-  } while(val < 65 && val > 122);
-
-    
-  if(mode == 's'){
-    speed = val;
-  }else if(mode == 't'){
-    direction = val;
-  }
-  Serial.print("val: "); Serial.println(val);
-  Serial.print("spd: "); Serial.println(speed);
-  Serial.print("dir: "); Serial.println(direction);
-  moveChar(speed, direction);
+  val = Serial.parseInt();
   
-}
-
-void moveChar(int velocityChar, int turnChar){   //Takes in values from 0 to 255 for velocity and turn
-  int convertedVel = map(velocityChar, 65, 172, -255, 255);  //Stretch a char's range to int for the function
-  int convertedTurn = map(turnChar, 65, 172, -255, 255);
-  move(convertedVel, convertedTurn);
+    
+  switch(mode){
+    case 'w': speed = val; direction = 0; break; //straight
+    case 'a': direction = -val; break; //left
+    case 's': speed = -val;  direction = 0; break; //backwards
+    case 'd': direction = val; break; //right
+    case 'z': speed = 0; direction = 0; break; //stop
+  }
+  
+  move(speed, direction);
+  
 }
 
 void move(int velocity, int turn){  //Velocity is the forward/back speed. Right is positive turn. left is negative
   int leftVelocity;
   int rightVelocity;
-
-
   
   //First, set both wheel values to the velocity (forward or backwards). The turn adjustments will be made next
   leftVelocity = velocity;    //The speed the left wheel turns
   rightVelocity = velocity;   //The speed the right wheel turns
-
   
-  //##TO DO: There is currently protection for values over 255 but not values under -255
   if(turn > 0){         //If we are turning right
     leftVelocity += turn;   //Adjust speed of left wheel so it turns right
     
@@ -105,7 +88,8 @@ void move(int velocity, int turn){  //Velocity is the forward/back speed. Right 
   
   //The rightVelocity and leftVelocity ints now have a value from -255 to 255
 
-
+  //Serial.print("leftVelocity: "); Serial.println(leftVelocity);
+  //Serial.print("rightVelocity: "); Serial.println(rightVelocity);
   
   if(leftVelocity > 0){   
     digitalWrite(leftDir, HIGH); analogWrite(leftSpd, leftVelocity); //Left forwards
@@ -126,14 +110,4 @@ void move(int velocity, int turn){  //Velocity is the forward/back speed. Right 
   else{
     digitalWrite(rightDir, LOW); analogWrite(rightSpd, 0); //Right stop
   }
-}
-
-void halt(){
-  speed = 128;
-  direction = 128;
-  moveChar(speed, direction);
-}
-void m(){
-  moveChar(speed, direction);
-  delay(80);
 }
