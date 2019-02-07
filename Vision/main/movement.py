b3063f235f727619c21f4de5065963f69fe61a34
Gable Brown <gbrown57@uncc.edu>
2019-02-06 19:27:09 -0500

    Added documentation for movement.py

--- Vision/main/movement.py
+++ Vision/main/movement.py
@@ -2,4 +2,31 @@
 
 This is where the code for communicating to the movement arduino will go.
 
+WSAD + Z followed by an int 0 to 255
+
+W
+A
+S
+D
+Z0 = STOP
+
 '''
+
+
+import smbus as smbus
+import time
+
+bus = smbus.SMBus(1)
+
+slave_address = 0x08
+
+def writeArray(value):
+    msg = []
+    for x in value:
+        msg.append(int(ord(x)))
+    print(msg)
+    bus.write_i2c_block_data(slave_address, 0, msg)
+        #bus.write_byte_data(slave_address, 0, ord(x))
+    return -1
+
+writeArray('Hello World, my name is Matt')
