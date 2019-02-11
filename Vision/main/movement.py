21e02b4152febf83e1fad66bea57ba4945784256
Unknown <dylan.fendrich@gmail.com>
2019-02-11 18:57:04 -0500

    added chase food function, returns x, speed ( speed based off y)

--- Vision/main/movement.py
+++ Vision/main/movement.py
@@ -13,20 +13,35 @@ Z0 = STOP
 '''
 
 
-import smbus2 as smbus
+import smbus2
 import time
 
-bus = smbus.SMBus(1)
+bus = smbus2.SMBus(1)
 
 slave_address = 0x08
 
-def writeArray(value):
-    msg = []
-    for x in value:
-        msg.append(int(ord(x)))
-    print(msg)
-    bus.write_i2c_block_data(slave_address, 0, msg)
-        #bus.write_byte_data(slave_address, 0, ord(x))
-    return -1
+# def writeArray(value):
+#     msg = []
+#     for x in value:
+#         msg.append(int(ord(x)))
+#     print(msg)
+#     bus.write_i2c_block_data(slave_address, 0, msg)
+#         #bus.write_byte_data(slave_address, 0, ord(x))
+#     return -1
 
-writeArray('Hello World, my name is Matt')
+def chasefood(x, y):
+    speed = 0
+    if(y < 100):
+        speed = 50
+    elif((y > 100) and (y < 200)):
+        speed = 75
+    elif((y > 200) and (y < 300)):
+        speed = 100
+    elif((y > 300) and (y < 400)):
+        speed = 125
+    elif((y > 400) and (y < 500)):
+        speed = 150
+    elif(y > 500):
+        speed = 150
+
+    return x, speed
