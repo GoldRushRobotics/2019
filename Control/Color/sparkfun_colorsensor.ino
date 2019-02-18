c711c790d3e32a75c1f7a2b32387c415a5651a0b
will7007 <38322012+will7007@users.noreply.github.com>
2019-02-18 18:26:24 -0500

    Added leds (not the backlight leds) and team color selection

--- Control/Color/sparkfun_colorsensor.ino
+++ Control/Color/sparkfun_colorsensor.ino
@@ -7,13 +7,13 @@ First code by: Jordan McConnell
 Heavily modified by William Daniels
 
 To Dew:
-Fix shiny ball reading
 Get LEDS for ball reading
 */
 
 #include <Servo.h>
 
 Servo myservo;
+//Note that the servo motor must be externally powered, having the Arduino power the servo causes errors in the analog color sensor
 
 const int AVERAGE_AMOUNT=20; //number of times to average color
 const int BASE_COLOR=100;
@@ -40,12 +40,33 @@ int redValue, greenValue, blueValue = 0;
   //GSX1 ground: Small drop
   //GSX0 and GSX 1 ground: large gain
   //Nothing needs to be tied to ground, though
+
+  const int REDLED=2;
+  const int BLUELED=3;
+  const int GREENLED=4;
+  const int YELLOWLED=5;
+
+  int value = 0;
+  bool isRB = false;
+  //If we are red and blue, we will want to sort green and yellow into the dumping bins
+  
 void setup() 
 {
   Serial.begin(9600);
 
   myservo.attach(9);
   myservo.write(0);
+
+  pinMode(REDLED,OUTPUT);
+  pinMode(BLUELED,OUTPUT);
+  pinMode(GREENLED,OUTPUT);
+  pinMode(YELLOWLED,OUTPUT);
+
+  value=analogRead(A3);
+
+  while(value > 100 && value < 150) {value=analogRead(A3);delay(50);}
+  isRB = (value <= 100) ? true : false;
+ 
 }
 
 void loop() 
@@ -88,39 +109,76 @@ void loop()
   Raverage /= AVERAGE_AMOUNT;
   Gaverage /= AVERAGE_AMOUNT;
   Baverage /= AVERAGE_AMOUNT;
-  /*Serial.print(Baverage);
+  Serial.print(Baverage);
   Serial.print(",");
   Serial.print(Raverage);
   Serial.print(",");
-  Serial.println(Gaverage);*/
+  Serial.println(Gaverage);
 
   if(Raverage > BASE_COLOR || Gaverage > BASE_COLOR || Baverage > BASE_COLOR) {
     if(Raverage > Gaverage && Raverage > Baverage) {
       if(abs(Baverage - Gaverage) >= 30) {
-        Serial.println("YELLOW");
-        if(reading != YELLOW) myservo.write(120);
-        reading=YELLOW;
+        //Serial.println("YELLOW");
+        
+        if(reading != YELLOW)  //120
+        {
+          myservo.write(0);
+          reading=YELLOW;
+          digitalWrite(REDLED,LOW);
+          digitalWrite(BLUELED,LOW);
+          digitalWrite(GREENLED,LOW);
+          digitalWrite(YELLOWLED,HIGH);
+        }
         }
       else {
-        Serial.println("RED");
-        if(reading != RED) myservo.write(90);
-        reading=RED;
+        //Serial.println("RED");
+        if(reading != RED) 
+        {
+          myservo.write(90);
+          reading=RED;
+          digitalWrite(REDLED,HIGH);
+          digitalWrite(BLUELED,LOW);
+          digitalWrite(GREENLED,LOW);
+          digitalWrite(YELLOWLED,LOW);
+        }
         }
     }
     else if(Gaverage > Raverage && Gaverage > Baverage) {
-      Serial.println("GREEN");
-      if(reading != GREEN) myservo.write(90);
-      reading=GREEN;
+      //Serial.println("GREEN");
+      if(reading != GREEN) 
+      {
+        myservo.write(180);
+        reading=GREEN;
+        digitalWrite(REDLED,LOW);
+        digitalWrite(BLUELED,LOW);
+        digitalWrite(GREENLED,HIGH);
+        digitalWrite(YELLOWLED,LOW);
+      }
+      
       }
     else {
-      Serial.println("BLUE");
-      if(reading != BLUE) myservo.write(60);
-      reading=BLUE;
+      //Serial.println("BLUE");
+      if(reading != BLUE) 
+      {
+        myservo.write(90);
+        reading=BLUE;
+        digitalWrite(REDLED,LOW);
+        digitalWrite(BLUELED,HIGH);
+        digitalWrite(GREENLED,LOW);
+        digitalWrite(YELLOWLED,LOW);
+      }
       }
   }  
       else {
-        Serial.println("WHAT???");
-        if(reading != NONE) myservo.write(90);
+        //Serial.println("WHAT???");
+        if(reading != NONE) 
+        {
+        myservo.write(90);        
         reading=NONE;
-        }
-  }
+        digitalWrite(REDLED,LOW);
+        digitalWrite(BLUELED,LOW);
+        digitalWrite(GREENLED,LOW);
+        digitalWrite(YELLOWLED,LOW);
+      }
+      }
+}
