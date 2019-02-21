d10bee049bce41475cc27916f60e010c6f8a4e09
Gable Brown <gbrown57@uncc.edu>
2019-02-20 22:04:49 -0500

    changed the port for serial

--- Vision/main/movement.py
+++ Vision/main/movement.py
@@ -19,7 +19,7 @@ import serial
 class mov:
 
     def __init__(self, w, h):
-        self.ser = None #serial.Serial(port='/dev/ttyS0', baudrate=115200, writeTimeout = 0)
+        self.ser = None #serial.Serial(port='/dev/ttyACM0', baudrate=115200, writeTimeout = 0)
 
         self.w = w
         self.halfW = w/2
