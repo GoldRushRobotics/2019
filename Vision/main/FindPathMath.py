7fe47ad0597ff98ec13eb0f9b47047e2bf586c25
NathanLangley <42824999+NathanLangley@users.noreply.github.com>
2019-03-14 13:42:27 -0400

    Path math uploaded
    updated with arcsin approximation function

---
+++ Vision/main/FindPathMath.py
@@ -0,0 +1,29 @@
+'''
+
+Math behind the path finding.
+
+'''
+import math
+
+Const = 1.0
+
+def findTurn(x, y, maxY, maxX, objgrav):
+	halfX = .5*maxX
+
+	if (x < halfX):
+		xsqr = -((x-halfX)/halfX *(x-halfX)/halfX)
+	else:
+		xsqr = (x-halfX)/halfX * (x-halfX)/halfX
+
+	ysqr = y/maxY * y/maxY
+
+	turn = xsqr * ysqr * objgrav * Const
+
+	return findPow(turn)
+
+
+def findPow(turn):
+	bpow = (1-(turn*turn)**.5
+	#theta = math.asin(turn)
+	theta = turn + (1/6 * turn**3) + (3/40 * turn**5) + (15/336 * turn**7) #approximation of arcsin(runs quicker) probably?
+	return (bpow*255, theta)
