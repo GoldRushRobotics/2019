3c96ad4ea56a0eb6cc02023eb7f288280cc91364
Gable Brown <gbrown57@uncc.edu>
2019-03-20 20:43:53 -0400

    TUPLE TUPLE TUPLE

--- Vision/main/HaarTest.py
+++ Vision/main/HaarTest.py
@@ -13,23 +13,25 @@ cube_cascade = cv2.CascadeClassifier('cube/cascade.xml')
 ball_cascade = cv2.CascadeClassifier('ball/cascade.xml')
 while True:
     ret, img = vs.read()
-    cv2.imshow('1', img)
+    img = cv2.resize(img, (int(vs.get(3)/4),int(vs.get(4)/4)))
+    # cv2.imshow('1', img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # add this
             # image, reject levels level weights.
-    balls = ball_cascade.detectMultiScale(gray, 2, 3)
-    cubes = cube_cascade.detectMultiScale(gray, 2, 3)
+    balls = ball_cascade.detectMultiScale(gray, 2, minNeighbors = 2, minSize = (200,200))
+    cubes = cube_cascade.detectMultiScale(gray, 2, minNeighbors = 2, minSize =  (200,200))
 
             # Ensure that output is a list
     if len(balls) == 0 and len(cubes) == 0:
-        print("")
+        objs = [[1,1,1,1]]
+        #print("")
     elif len(balls) == 0:
         objs = cubes
-        cv2.circle(img, (objs[0][0], objs[0][1]), 50, (255, 255, 255), 2)
+        # cv2.circle(img, (objs[0][0], objs[0][1]), 50, (255, 255, 255), 2)
     elif len(cubes) == 0:
         objs = balls
-        cv2.circle(img, (objs[0][0], objs[0][1]), 50, (255, 255, 255), 2)
+        # cv2.circle(img, (objs[0][0], objs[0][1]), 50, (255, 255, 255), 2)
     else:
                 # combine into a vStack
         objs = np.vstack((balls, cubes))
@@ -38,7 +40,10 @@ while True:
             # Sort in place wasnt working, dont @ me
 
         objs = sorted(objs, reverse=True, key=lambda x: x[3])
-        cv2.circle(img, (objs[0][0], objs[0][1]), 50, (255, 255, 255), 2)
+
+    for (x,y,w,h) in objs:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
     cv2.imshow('0',img)
     k = cv2.waitKey(100) & 0xFF # large wait time to remove freezing
     if k == 113 or k == 27:
