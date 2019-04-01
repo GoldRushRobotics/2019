4d52b8050983a9420701047c01bb4ab2f66a903c
Gable Brown <gbrown57@uncc.edu>
2019-03-31 22:08:18 -0400

    Added the new cascades, made some changes to testing files

--- Vision/main/HaarTest.py
+++ Vision/main/HaarTest.py
@@ -2,33 +2,40 @@ import cv2
 import numpy as np
 import time
 
-vs = cv2.VideoCapture(0)
-#width = 600
-#height = 600
+vs = cv2.VideoCapture('vid_test.MOV')
+# width = 600
+# height = 600
 
 time.sleep(2)
+
+w = int(vs.get(3))
+h = int(vs.get(4))
+
+print("{0} {1}".format(w, h))
 
 
 cube_cascade = cv2.CascadeClassifier('Cube/cascade.xml')
 ball_cascade = cv2.CascadeClassifier('Ball/cascade.xml')
+
 while True:
     ret, img = vs.read()
-    #img = cv2.GaussianBlur(img, (3, 3), 2)
-    #img = cv2.flip(img, 0)
-    img = cv2.resize(img, (512, 512))
+
+    # img = cv2.GaussianBlur(img, (3, 3), 2)
+    # img = cv2.flip(img, 0)
+    img = cv2.resize(img, (480, 270))
     # cv2.imshow('1', img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # add this
     # image, reject levels level weights.
     balls = ball_cascade.detectMultiScale(
-        gray, 2, minNeighbors=1, minSize=(16, 16))
+        gray, 2.5, minNeighbors=5, minSize=(64, 64), maxSize=(128, 128))
     cubes = cube_cascade.detectMultiScale(
-        gray, 2, minNeighbors=1, minSize=(16, 16))
+        gray, 3.5, minNeighbors=5, minSize=(64, 64), maxSize=(128, 128))
 
     # Ensure that output is a list
     if len(balls) == 0 and len(cubes) == 0:
-        objs = []
+        objs = [(1, 1, 1, 1)]
         print("None")
     elif len(balls) == 0:
         objs = cubes
@@ -47,9 +54,14 @@ while True:
         # Sort in place wasnt working, dont @ me
 
     objs = sorted(objs, reverse=True, key=lambda x: x[3])
-    print(len(objs))
-    for (x, y, w, h) in objs:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+    (x, y, w, h) = objs[0]
+
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+    # print(len(objs))
+    # for (x, y, w, h) in objs:
+    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
     cv2.imshow('0', img)
     k = cv2.waitKey(100) & 0xFF  # large wait time to remove freezing
--- Vision/opencv_workspace/training/vid2img.py
+++ Vision/opencv_workspace/training/vid2img.py
@@ -5,95 +5,127 @@ import os
 import time
 import re
 import sys
+import threading
+import psutil
 # from imutils.video import VideoCapture
-#vs = cv2.VideoCapture('training/IMG_0024.MOV')
+
 
 outPath = sys.argv[2]
 inPath = sys.argv[1]
-#time.sleep(2)
+# time.sleep(2)
+
+
+class myThread(threading.Thread):
+
+    def __init__(self, threadID, current_frame, outPath):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.current_frame = current_frame
+        self.outPath = outPath
+
+    def run(self):
+        print('{0}.jpg'.format(self.threadID))
+        self.gray = cv2.cvtColor(self.current_frame, cv2.COLOR_BGR2GRAY)
+        self.gray = cv2.resize(self.gray, (240, 135))
+        #cv2.imshow('preview', gray)
+        cv2.imwrite(self.outPath + str(self.threadID) + '.jpg', self.gray)
 
 
 if not os.path.exists(outPath):
-  os.makedirs(outPath)
-
-def captureVideo(vs,outPath):
+    os.makedirs(outPath)
 
 
-  num = 200000
+def captureVideo(inPath, outPath):
+
+    vs = cv2.VideoCapture(inPath)
+
+    process = psutil.Process(os.getpid())
+
+    num = 58352
+
+    while(True):
+        # Capture frame-by-frame
+        # if process.memory_info().rss >= 40000000000:
+        #     time.sleep(1)
+        #     print(process.memory_info().rss)
+        #     continue
+
+        ret, current_frame = vs.read()
+        if type(current_frame) == type(None):
+            print("noFrame")
+            break
+
+        print('{0}.jpg'.format(num))
+        gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.resize(gray, (240, 135))
+        #cv2.imshow('preview', gray)
+        cv2.imwrite(outPath + str(num) + '.jpg', gray)
+
+        # thread = myThread(num, current_frame, outPath)
+
+        # thread.start()
+
+        # exec('thread{0} = thread'.format(num))
+
+        num += 1
+        # Display the resulting frame
+
+        # if cv2.waitKey(1) & 0xFF == ord('c'):
+        #     # cv2.imshow('captured',gray)
+        #     cv2.imwrite(path+'/'+str(num)+'.jpg',gray)
+        #     num += 1
+        #     # time.sleep(.2)
+        #     # cv2.destroyAllWindows()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-  while(True):
-    # Capture frame-by-frame
-    ret, current_frame = vs.read()
-    if type(current_frame) == type(None):
-        print("noFrame")
-        break
-
-
-    gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (128, 128))
-    cv2.imshow('preview',gray)
-    cv2.imwrite(outPath+str(num)+'.jpg',gray)
-    num += 1
-    # Display the resulting frame
-
-    # if cv2.waitKey(1) & 0xFF == ord('c'):
-    #     # cv2.imshow('captured',gray)
-    #     cv2.imwrite(path+'/'+str(num)+'.jpg',gray)
-    #     num += 1
-    #     # time.sleep(.2)
-    #     # cv2.destroyAllWindows()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 def bitand(inpath):
     files = os.listdir(inPath)
 
-    mask = cv2.imread(inPath+files[0],0)
+    mask = cv2.imread(inPath + files[0], 0)
 
     for file in files:
-        gray = cv2.imread(inPath+file,0)
+        gray = cv2.imread(inPath + file, 0)
         mask = cv2.bitwise_and(gray, mask)
 
-    cv2.imshow("blah",mask)
+    cv2.imshow("blah", mask)
 
 
-def fromImgs(inPath,outPath):
+def fromImgs(inPath, outPath):
+
+    num = 58352
+
+    files = os.listdir(inPath)
+
+    for file in files:
+        print(inPath + file)
+        # Capture frame-by-frame
+        gray = cv2.imread(inPath + file, 0)
+        # ret, current_frame = vs.read()
+        # if type(current_frame) == type(None):
+        #     print("noFrame")
+        #     break
+
+        # gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.resize(gray, (64, 64))
+        # cv2.imshow('preview',gray)
+        cv2.imwrite(outPath + str(num) + '.png', gray)
+        num += 1
+        # Display the resulting frame
+
+        # if cv2.waitKey(1) & 0xFF == ord('c'):
+        #     # cv2.imshow('captured',gray)
+        #     cv2.imwrite(path+'/'+str(num)+'.jpg',gray)
+        #     num += 1
+        #     # time.sleep(.2)
+        #     # cv2.destroyAllWindows()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-  num = 20
-
-
-  files = os.listdir(inPath)
-
-  for file in files:
-    print(inPath+file)
-    # Capture frame-by-frame
-    gray = cv2.imread(inPath+file,0)
-    # ret, current_frame = vs.read()
-    # if type(current_frame) == type(None):
-    #     print("noFrame")
-    #     break
-
-
-    # gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (64, 64))
-    #cv2.imshow('preview',gray)
-    cv2.imwrite(outPath+str(num)+'.png',gray)
-    num += 1
-    # Display the resulting frame
-
-    # if cv2.waitKey(1) & 0xFF == ord('c'):
-    #     # cv2.imshow('captured',gray)
-    #     cv2.imwrite(path+'/'+str(num)+'.jpg',gray)
-    #     num += 1
-    #     # time.sleep(.2)
-    #     # cv2.destroyAllWindows()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-bitand(outPath)
-#fromImgs(inPath,outPath)
-#captureVideo(vs,outPath)
+# bitand(outPath)
+# fromImgs(inPath,outPath)
+captureVideo(inPath, outPath)
