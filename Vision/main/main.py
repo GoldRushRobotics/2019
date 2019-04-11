42fcab7b392d82abcee5ef9fceb316f3290a2189
Gable Brown <gbrown57@uncc.edu>
2019-04-10 20:05:25 -0400

    testing4

--- Vision/main/main.py
+++ Vision/main/main.py
@@ -37,8 +37,8 @@ def setup():
 
     (w, h) = (64, 36)
 
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 64)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 36)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
 
     # Initialize Panduino communications
     movmt = mov(w, h)
@@ -58,6 +58,9 @@ def setup():
     # Calculate the home color from the first frame
     homeColor = findColor(firstFrame, 10, 10, (int(w / 2) - 5), h)
 
+    # Save image for checking
+    cv2.imwrite("firstFrame.jpg", firstFrame)
+
     # Explicitly remove first frame to increase free space
     del(firstFrame)
 
@@ -67,8 +70,26 @@ def setup():
     # Send home color to Panduino
     movmt.writeArray("h{0}".format(homeColor))
 
+
+def die():
+
+    # If there is a keyboard interrupt, tell Panduino to stop
+    movmt.writeArray('a0')
+    movmt.writeArray('w0')
+    movmt.writeArray('z0')
+
+    # Destroy all cv2 windows
+    cv2.destroyAllWindows()
+
+    # Close the serial connection
+    movmt.ser.close()
+
+    # Exit python
+    exit()
+
 if __name__ == "__main__":
     setup()
+    die()
 
     try:
 
@@ -96,17 +117,4 @@ if __name__ == "__main__":
             movmt.whereToGo(pill, tels)
 
     except (KeyboardInterrupt, ValueError) as e:
-
-        # If there is a keyboard interrupt, tell Panduino to stop
-        movmt.writeArray('a0')
-        movmt.writeArray('w0')
-        movmt.writeArray('z0')
-
-        # Destroy all cv2 windows
-        cv2.destroyAllWindows()
-
-        # Close the serial connection
-        movmt.ser.close()
-
-        # Exit python
-        exit()
+        die()
